34a599cc83189e4c53806e4afd1b55f5487cb377
MTL1027 <dhanushbunny8@gmail.com>
2025-05-16 16:22:21 +0530

    first

--- src/main/java/com/accesshr/emsbackend/EmsBackendApplication.java
+++ src/main/java/com/accesshr/emsbackend/EmsBackendApplication.java
@@ -3,6 +3,7 @@ package com.accesshr.emsbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
@@ -11,6 +12,11 @@ public class EmsBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmsBackendApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello from App Engine!";
     }
 
 }
